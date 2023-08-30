baf50acc7b27704d4527b82c6389c0d1e763d304
Kaybangz <devcaleb96@gmail.com>
2023-08-30 13:01:33 +0100

    Created 404 page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponentComponent } from './components/page-not-found-component/page-not-found-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponentComponent
   ],
   imports: [
     BrowserModule,
