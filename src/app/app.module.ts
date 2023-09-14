25b4945b2876f80d66212e5addc70ae4074a313a
Kaybangz <devcaleb96@gmail.com>
2023-09-14 18:30:08 +0100

    Created all of my app's components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PageNotFoundComponentComponent } from './components/page-not-found-component/page-not-found-component.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+import { SignInFormComponent } from './sign-in-form/sign-in-form.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { EventsComponent } from './events/events.component';
+import { EventComponent } from './event/event.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponentComponent
+    HomeComponent,
+    NavbarComponent,
+    SignUpFormComponent,
+    SignInFormComponent,
+    UserDashboardComponent,
+    AdminDashboardComponent,
+    HeroSectionComponent,
+    EventsComponent,
+    EventComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
